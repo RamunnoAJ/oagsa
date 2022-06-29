2a976e4b152a62013b726a972355fc54196124f4
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-29 14:28:07 -0300

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <Navigation />
   <RouterView />
+  <Footer />
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
 import Navigation from './components/Navigation.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <div class="bg-slate-100 min-h-min py-3 mt-3">
+      <div class="max-w-5xl grid gap-4 grid-cols-3 mx-auto justify-items-center">
+        <div>
+          <div class="flex items-center flex-shrink-0 mr-6">
+            <img class="fill-current lg:w-32 mr-2" src="../assets/images/logo.png" alt="branding logo">
+          </div>
+          <div>
+            <p class="font-thin">OSCAR A. GONZALEZ S.A. es una empresa especializada en la importación y distribución
+              mayorista de
+              repuestos para Ciclomotores, Scooters, Motos, Cuatriciclos y Bicicletas.</p>
+          </div>
+        </div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+    </div>
+    <div class="bg-sky-100 text-xs py-2">
+      <div class="container flex sm:justify-center space-x-4 text-gray-600 gap-4 items-center m-auto">
+        <p>Copyright © 2017 - 2022 OSCAR A. GONZALEZ S.A. All rights reserved.</p>
+      </div>
+    </div>
+  </footer>
+</template>
