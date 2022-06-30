4b21ecd7aee38271553c4bcb119f5f3ddecd6715
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-30 17:13:20 -0300

    added companyview + images

---
+++ src/views/CompanyView.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="container mx-auto my-8">
+    <h1 class="text-center text-3xl sm:text-5xl text-sky-600">EMPRESA</h1>
+    <hr class="m-8 sm:m-14 bg-sky-600 h-0.5">
+    <div class="grid grid-cols-1 gap-4 mx-2 mt-8 md:gap-8 lg:gap-14 sm:grid-cols-2">
+      <div class="text-center"><img src="../assets/images/img-empresa-1.jpg" alt="" class="w-full"></div>
+      <div class="text-left text-sm sm:text-base py-2">
+        <p>OSCAR A. GONZALEZ S.A. es una empresa especializada en la importación y distribución mayorista de repuestos
+          para Ciclomotores, Scooters, Motos, Cuatriciclos y Bicicletas. <br>Cuenta con una organización de ventas,
+          administración, expedición y logística altamente profesionalizadas, que asegura a los clientes una rápida y
+          segura respuesta a sus requerimientos.</p>
+      </div>
+      <div class="text-left text-sm sm:text-base py-2">
+        <p>Su filosofía de trabajo desde hace más de treinta años, es lograr "La excelencia del Servicio" . En
+          cumplimiento de este eslogan, es que representamos y distribuimos productos de primera calidad a nivel
+          mundial.</p>
+      </div>
+      <div class="text-center"><img src="../assets/images/img-empresa-2.jpg" alt="" class="w-full"></div>
+    </div>
+
+  </section>
+</template>
