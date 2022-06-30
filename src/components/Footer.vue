ccb3998636d552fa0f9c4e22ae11be9d41044de0
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-30 17:13:42 -0300

    added map and social links to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,23 +1,35 @@
 <template>
   <footer>
     <div class="bg-slate-100 min-h-min py-3 mt-3">
-      <div class="max-w-5xl grid gap-4 grid-cols-3 mx-auto justify-items-center">
+      <div class="max-w-5xl grid gap-4 grid-cols-1 mx-auto justify-items-center sm:grid-cols-3 px-5">
         <div>
-          <div class="flex items-center flex-shrink-0 mr-6 justify-center">
-            <img class="fill-current lg:w-32 mr-2" src="../assets/images/logo.png" alt="branding logo">
+          <div class="flex items-center flex-shrink-0 mr-6 justify-center mb-4">
+            <img class="fill-current w-20 mr-2" src="../assets/images/logo.png" alt="branding logo">
           </div>
           <div>
-            <p class="font-thin">OSCAR A. GONZALEZ S.A. es una empresa especializada en la importación y distribución
+            <p class="font-thin text-sm">OSCAR A. GONZALEZ S.A. es una empresa especializada en la importación y
+              distribución
               mayorista de
               repuestos para Ciclomotores, Scooters, Motos, Cuatriciclos y Bicicletas.</p>
           </div>
         </div>
-        <div>02</div>
-        <div>03</div>
+        <div class="flex sm:flex-col justify-center items-center">
+          <a href="https://www.facebook.com/profile.php?id=100063653886728" target="_blank"
+            class="text-sky-600 hover:text-black p-1 ease-in-out duration-300 text-xl p-2">
+            <i class="fa-brands fa-facebook-f"></i>
+          </a>
+          <a href="https://www.instagram.com/oag.s.a/" target="_blank"
+            class="text-sky-600 hover:text-black ease-in-out duration-300 text-xl p-2">
+            <i class="fa-brands fa-instagram p-2"></i>
+          </a>
+        </div>
+        <div class="m-4 border-2 border-sky-300"><iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3143.574394431887!2d-57.589137048557745!3d-38.010386652994235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9584decfdbc06d8b%3A0x46ef2fd928e1e478!2sCeferino%20Namuncura%20232%2C%20B7608EQF%20Mar%20del%20Plata%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1656617716550!5m2!1ses-419!2sar"
+            loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
       </div>
     </div>
     <div class="bg-sky-100 text-xs py-2">
-      <div class="container flex sm:justify-center space-x-4 text-gray-600 gap-4 items-center m-auto">
+      <div class="container flex sm:justify-center space-x-4 text-gray-600 gap-4 items-center m-auto p-4">
         <p>Copyright © 2017 - 2022 OSCAR A. GONZALEZ S.A. All rights reserved.</p>
       </div>
     </div>
