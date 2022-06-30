aa6ac7297a0429532f3e0c4a3b96ccab432c694a
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-30 14:20:07 -0300

    updated carrousel

--- src/components/Carrousel.vue
+++ src/components/Carrousel.vue
@@ -1,20 +1,41 @@
 <template>
-  <div class="container mx-auto">
+  <div class="container mx-auto px-4">
     <swiper class="swiper" :modules="modules" :space-between="30" :centered-slides="true" :navigation="true"
       :pagination="{
         clickable: true
       }" :autoplay="{
-  delay: 2500,
+  delay: 3000,
   disableOnInteraction: false
 }">
-      <swiper-slide class="slide">Slide 1</swiper-slide>
-      <swiper-slide class="slide">Slide 2</swiper-slide>
-      <swiper-slide class="slide">Slide 3</swiper-slide>
-      <swiper-slide class="slide">Slide 4</swiper-slide>
-      <swiper-slide class="slide">Slide 5</swiper-slide>
-      <swiper-slide class="slide">Slide 6</swiper-slide>
-      <swiper-slide class="slide">Slide 7</swiper-slide>
-      <swiper-slide class="slide">Slide 8</swiper-slide>
+      <swiper-slide class="slide flex justify-around items-center pr-5"><img src="../assets/images/slides/slide-1.png"
+          class="h-80">
+        <p class="text-3xl">REDES SOCIALES</p>
+      </swiper-slide>
+      <swiper-slide class="slide flex justify-around items-center pr-5"><img src="../assets/images/slides/slide-2.png"
+          class="h-80">
+        <p class="text-3xl">RALCO <br> <span class="text-base">DISTRIBUIDOR EXCLUSIVO</span> <br> <span
+            class="text-sm">NEUMATICOS DE
+            ORIGEN INDIO</span>
+        </p>
+      </swiper-slide>
+      <swiper-slide class="slide flex justify-around items-center pr-5"><img src="../assets/images/slides/slide-3.png"
+          class="h-80">
+        <p class="text-4xl">LUBRICANTE</p>
+      </swiper-slide>
+      <swiper-slide class="slide flex justify-around items-center pr-5"><img src="../assets/images/slides/slide-4.png"
+          class="h-80">
+        <p class="text-4xl">NEUMÁTICOS <br> <span class="text-base">AUTOS, CAMIONETAS, CAMIONES...</span></p>
+      </swiper-slide>
+      <swiper-slide class="slide flex justify-around items-center pr-5"><img src="../assets/images/slides/slide-5.png"
+          class="h-80">
+        <p class="text-4xl">METRO <br> <span class="text-base">CUBIERTAS BICICLETAS</span></p>
+      </swiper-slide>
+      <swiper-slide class="slide flex justify-around items-center pr-5"><img src="../assets/images/slides/slide-6.png"
+          class="h-80">
+        <p class="text-4xl">METRO <br> <span class="text-base">CAMARAS BICICLETAS</span> <br> <span
+            class="text-sm">VALVULA DE AUTO</span></p>
+      </swiper-slide>
+
     </swiper>
   </div>
 
