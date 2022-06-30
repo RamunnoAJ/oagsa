576b7f4d64c3b4128cef193019ad644af66ae228
RamunnoAJ <ramunnoagustin@gmail.com>
2022-06-30 16:03:06 -0300

    created all views

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import CompanyView from '../views/CompanyView.vue';
+import CatalogoView from '../views/CatalogoView.vue';
+import NewsView from '../views/NewsView.vue';
+import ContactView from '../views/ContactView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'inicio',
       component: HomeView,
+    },
+    {
+      path: '/empresa',
+      name: 'empresa',
+      component: CompanyView,
+    },
+    {
+      path: '/catalogo',
+      name: 'catalogo',
+      component: CatalogoView,
+    },
+    {
+      path: '/novedades',
+      name: 'novedades',
+      component: NewsView,
+    },
+    {
+      path: '/contactenos',
+      name: 'contactenos',
+      component: ContactView,
     },
   ],
 });
